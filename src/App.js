f45836b20c3c51efc7e93440adffeb31f569efdc
Roman-Dumych <Roman.Dumych96@gmail.com>
2023-04-22 23:45:18 +0300

    home-work_2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import './App.css';
+import {Posts} from "./components/Posts";
+import {Launches} from "./components/Launches";
+import Users from "./components/Users";
+import {useState} from "react";
+import UserPosts from "./components/UserPosts";
 
-function App() {
-  return (
-    <div className="App">
-      hw2
 
-    </div>
-  );
-}
+const App = () => {
+    const [userId, setUserId] = useState(null);
+    return (
+        <div>
+            {/*<Posts/>*/}
+            {/*<Launches/>*/}
+            <Users setUserId={setUserId}/>
+            {userId && <UserPosts userId={userId}/>}
+        </div>
+    );
+};
 
 export default App;
